566359129c688d10093e5987d311638b6a6cfa94
samtaylek17 <samtaylek@gmail.com>
2020-11-12 11:39:47 +0100

    Messed things up a little bit more

--- app.js
+++ app.js
@@ -57,16 +57,17 @@ app.use((req, res, next) => {
 });
 
 // app.use('/', viewRouter);
+app.use('/api/messages', messageRouter);
+
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
 	//Set static folder
 	app.use(express.static('client/build'));
 
-	app.get('/', (req, res) => {
+	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
-app.use('/api/messages', messageRouter);
 
 // app.all('*', (req, res, next) => {
 //     res.status(404).render('error-page', {
